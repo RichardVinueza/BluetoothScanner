a19ab334a01490b074ad25e38f4df1cd483beaba
Richard Vinueza <richardvinueza@gmail.com>
2020-05-14 17:26:01 +0100

    HTML & TS show DeviceNumber

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,23 +8,28 @@ import { BLE } from "@ionic-native/ble/ngx";
 })
 export class HomePage {
   devices: any[] = [];
+  deviceNum: number;
 
   constructor(private ble: BLE, private ngZone: NgZone) {}
 
-  Scan(){
+  Scan() {
     this.devices = [];
-    this.ble.startScan([]).subscribe(
-      device => this.onDeviceDiscovered(device)
-    );
-  }
-  
-  onDeviceDiscovered(device){
-    console.log('Discovered' + JSON.stringify(device,null,2));
-    this.ngZone.run(()=>{
-      this.devices.push(device)
-      //console.log(device)
-      console.log("DEVICES: " + JSON.stringify(this.devices,null,2));
-    })
+    this.ble.startScan([]).subscribe((device) => {
+      console.log("Discovered" + JSON.stringify(device, null, 2));
+      this.ngZone.run(() => {
+        this.deviceNum = this.devices.push(device);
+        //console.log(device)
+        console.log("DEVICES: " + JSON.stringify(this.devices, null, 2));
+      });
+    });
   }
 
+  // onDeviceDiscovered(device) {
+  //   console.log("Discovered" + JSON.stringify(device, null, 2));
+  //   this.ngZone.run(() => {
+  //     this.devices.push(device);
+  //     //console.log(device)
+  //     console.log("DEVICES: " + JSON.stringify(this.devices, null, 2));
+  //   });
+  // }
 }
